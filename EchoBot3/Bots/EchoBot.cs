e240ec984401b433eae102de1308aa5cf487e178
pratik-sanu <72443544+pratik-sanu@users.noreply.github.com>
2020-10-07 11:43:52 +0530

    Update EchoBot.cs

--- EchoBot3/Bots/EchoBot.cs
+++ EchoBot3/Bots/EchoBot.cs
@@ -21,7 +21,7 @@ namespace EchoBot3.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hello and welcome!";
+            var welcomeText = "Hi !! What Can I do for you !";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
