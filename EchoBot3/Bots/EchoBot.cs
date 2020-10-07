d5e5518768da72c86f5f57d44905abaafed53b84
pratik-sanu <72443544+pratik-sanu@users.noreply.github.com>
2020-10-07 12:24:06 +0530

    Update EchoBot.cs

--- EchoBot3/Bots/EchoBot.cs
+++ EchoBot3/Bots/EchoBot.cs
@@ -15,7 +15,7 @@ namespace EchoBot3.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var replyText = $"Echo: {turnContext.Activity.Text}";
+            var replyText = $"{turnContext.Activity.Text}";
             await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
         }
 
