c65a721270bd70945223f62abd21b82dfa629d3a
pratik-sanu <72443544+pratik-sanu@users.noreply.github.com>
2020-10-08 19:57:29 +0530

    Update EchoBot.cs

--- EchoBot3/Bots/EchoBot.cs
+++ EchoBot3/Bots/EchoBot.cs
@@ -16,10 +16,16 @@ namespace EchoBot3.Bots
     public class EchoBot : ActivityHandler
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
-        {
-            var replyText = $"{turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        { await turnContext.SendActivityAsync(MessageFactory.Text($"Echo: {turnContext.Activity.Text}"), cancellationToken);
+
+   await AccessQnAMaker(turnContext, cancellationToken);
         }
+        public QnAMaker EchoBotQnA { get; private set; }
+            public EchoBot(QnAMakerEndpoint endpoint)
+            {
+            // connects to QnA Maker endpoint for each turn
+            EchoBotQnA = new QnAMaker(endpoint);
+            }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
@@ -32,5 +38,18 @@ namespace EchoBot3.Bots
                 }
             }
         }
+    private async Task AccessQnAMaker(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+{
+   var results = await EchoBotQnA.GetAnswersAsync(turnContext);
+   if (results.Any())
+   {
+      await turnContext.SendActivityAsync(MessageFactory.Text("QnA Maker Returned: " + results.First().Answer), cancellationToken);
+   }
+   else
+   {
+      await turnContext.SendActivityAsync(MessageFactory.Text("Sorry, could not find an answer in the Q and A system."), cancellationToken);
+   }
+}
     }
+    
 }
